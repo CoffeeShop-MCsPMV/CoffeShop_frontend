c69ac2a987ec273350512e0670837d8b442650ca
Mladoneczki <padi.mladoneczki@gmail.com>
2025-04-22 01:24:52 +0200

    mixerIMG működik

--- src/components/MixerImg.js
+++ src/components/MixerImg.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { MixerContext } from "../context/mixerContext";
 
 function MixerImg() {
+  const { addedIngredientList, ice } = useContext(MixerContext);
   return (
-    <div className='mixer-img'>
-       <img src="/images/italok-03.svg" alt="Espresso Karamell" />
-
+    <div className="mixer-img">
+      <img
+        src={`/images/mixer/${addedIngredientList[0].product_id}_${addedIngredientList[3].product_id}_${addedIngredientList[2].product_id}.svg`}
+        alt="Mixed Drink"
+      />
+      <div className="milk">
+        <img
+          src={`/images/mixer/${addedIngredientList[1].product_id}.png`}
+          alt=""
+        />
+      </div>
+      {ice && (
+        <div className="ice">
+          <img src="/images/mixer/ice.png" alt="Ice" />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default MixerImg
+export default MixerImg;
