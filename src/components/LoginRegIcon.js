13cc455f3f1d6508b13d4d580d7d44bd4c31f07c
chillag09733 <chillag09733@gmail.com>
2025-04-27 13:33:45 +0200

    loginregicon mód

--- src/components/LoginRegIcon.js
+++ src/components/LoginRegIcon.js
@@ -1,37 +1,33 @@
-import React, { useContext } from 'react'
-import useAuthContext from '../context/AuthContext';
+import React, { useContext } from "react";
+import useAuthContext from "../context/AuthContext";
 import "../style/Nav.css";
-import { Link } from 'react-router-dom';
-import { ApiContext } from '../context/apiContext';
+import { Link } from "react-router-dom";
+import { ApiContext } from "../context/apiContext";
 
 function LoginRegIcon() {
-     const { isMobile } = useContext(ApiContext);
-    const {setShowModal}=useAuthContext();
-    const handleLoginClick = () => {
-        setShowModal(true); // A modális ablakot megjelenítjük
-      };
+  const { isMobile } = useContext(ApiContext);
+  const { setShowModal } = useAuthContext();
+  const handleLoginClick = () => {
+    setShowModal(true); // A modális ablakot megjelenítjük
+  };
   return (
-    <div className={`login-reg-icon ${isMobile ? ' mobile' : ''}`}>
-    <ul className="navbar-nav">
-            <li className="navbar-item nav-login-register">
-              <Link
-                className="nav-link btn"
-                to="/login"
-                onClick={handleLoginClick}
-              >
-                Login
-              </Link>
-              <Link
-                className="nav-link btn"
-                to="/register"
-                onClick={handleLoginClick}
-              >
-                Registration
-              </Link>
-            </li>
-          </ul>
-          </div>
-  )
+    <div className={`login-reg-icon ${isMobile ? " mobile" : ""}`}>
+      <ul className="navbar-nav">
+        <li className="navbar-item nav-login-register">
+          <Link className="nav-link btn" to="/login" onClick={handleLoginClick}>
+            Login
+          </Link>
+          <Link
+            className="nav-link btn"
+            to="/register"
+            onClick={handleLoginClick}
+          >
+            Registration
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default LoginRegIcon
+export default LoginRegIcon;
