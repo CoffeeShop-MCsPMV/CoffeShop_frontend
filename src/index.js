a3201266c070ccc0d285f6c70b0eeff0e65c3948
Mladoneczki <padi.mladoneczki@gmail.com>
2025-04-30 03:00:26 +0200

    page 1.2

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { MixerProvider } from "./context/mixerContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter  basename="/CoffeShop_frontend">
+    <BrowserRouter>
       <AuthProvider>
         <ApiProvider>
           <CartProvider>
